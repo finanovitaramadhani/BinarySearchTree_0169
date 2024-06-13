e0bb806a3f9ff05b2585b5df888916231fa9b6b9
finanovita <finanovita479@gmail.com>
2024-06-13 13:32:02 +0700

    menambahkan prosedur void postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -103,6 +103,20 @@ public:
         }
     }
 
+    void postorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
 
 
