ef30c5f2bc984196258f40c4bf7791510e3d21aa
finanovita <finanovita479@gmail.com>
2024-06-13 13:25:44 +0700

    menambahakan prosedur void search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -57,6 +57,11 @@ public:
             parent->rightchild = newNode; // make the right child of the parent point to the new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+
+    }
 };
 
 
